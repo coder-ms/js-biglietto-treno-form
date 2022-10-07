b133d1107f5425c8c3321cb3c17b8df5b2995901
coder-ms <marco.sbrascia@gmail.com>
2022-10-07 15:03:49 +0300

    milestone 1: form  inserimento dati e codice base

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/*
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+
+
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+*/
+
+
+
+document.getElementById("calculate").onclick = function(){
+    var agePassenger = document.getElementById("passengerAge").value;
+    var distanceKm = document.getElementById("kmTrain").value;
+    const costTicket = 0.21;
+    if(agePassenger < 18 ){
+        costTrip = distanceKm * costTicket * 0.8;
+    }
+    else if(agePassenger > 65){
+        costTrip = distanceKm * costTicket * 0.6;
+    }
+    else{
+        costTrip = distanceKm * costTicket;
+    }
+    let totalCost = costTrip.toFixed(2);
+    console.log('Age of the passenger: ' + agePassenger + 'years old');
+    console.log('Distance traveled : ' + distanceKm + 'Km');
+    
+    console.log('Total : ' + totalCost);
+    document.getElementById("cost").innerHTML = totalCost;   
+}
+
